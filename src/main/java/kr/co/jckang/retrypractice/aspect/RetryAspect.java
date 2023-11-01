47e48ffa94dc8fb1949a297e3b29ecf83e0f04b3
jckang <jckang568@gmail.com>
2023-11-01 18:17:31 +0900

    #Fix: "AOP 내부 로직 구현중"
    AOP 사용법 숙지 중

--- src/main/java/kr/co/jckang/retrypractice/aspect/RetryAspect.java
+++ src/main/java/kr/co/jckang/retrypractice/aspect/RetryAspect.java
@@ -1,6 +1,7 @@
 package kr.co.jckang.retrypractice.aspect;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import kr.co.jckang.retrypractice.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -13,11 +14,14 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Component
 @Aspect
 public class RetryAspect {
+    private int callCount;
     @AfterThrowing(value = "@annotation(kr.co.jckang.retrypractice.annotation.Retry)"
             , throwing = "ex")
     public void afterThrowingAdvice(JoinPoint joinPoint
@@ -27,7 +31,9 @@ public class RetryAspect {
         Method method = methodSignature.getMethod();
 
         Retry retryAnnotation = method.getAnnotation(Retry.class);
-        // HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        HttpSession session = request.getSession();
+
 
         if (retryAnnotation != null) {
             int attempts = retryAnnotation.attempts();
@@ -35,12 +41,30 @@ public class RetryAspect {
             int backoff = retryAnnotation.backoff();
             Class<? extends Throwable>[] retryFor = retryAnnotation.value();
 
-            System.out.println("Retry Attempts: " + attempts);
-            System.out.println("Retry Delay: " + delay);
-            System.out.println("Retry Backoff: " + backoff);
-            System.out.println("Retry For Exceptions: " + Arrays.toString(retryFor));
+            containsExceptionClass(ex, retryFor);
+
+            for (int i = 0; i < attempts; i++) {
+                joinPoint.getSignature().
+            }
+
+//            System.out.println("Retry Attempts: " + attempts);
+//            System.out.println("Retry Delay: " + delay);
+//            System.out.println("Retry Backoff: " + backoff);
+//            System.out.println("Retry For Exceptions: " + Arrays.toString(retryFor));
+
+
         }
         System.out.println("After Throwing exception in method:" + joinPoint.getSignature());
         System.out.println("Exception is:" + ex.getMessage());
+    }
+
+    private static boolean containsExceptionClass(Exception ex, Class<? extends Throwable>[] retryFor) {
+        for (Class<? extends Throwable> retryExceptionClass : retryFor) {
+            if (ex.getClass().hashCode() == retryExceptionClass.hashCode()) {
+                System.out.println("Exception matches with retryFor: " + retryExceptionClass);
+                return true;
+            }
+        }
+        return false;
     }
 }
